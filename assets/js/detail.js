37f9a37d2c1f0a8a2928e5243f1cf82bc1fce4c5
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2024-01-11 22:55:33 -0300

    * Criação do design das páginas About e Base Stats. * Implementação da rotina para troca de abas.

--- assets/js/detail.js
+++ assets/js/detail.js
@@ -1,2 +1,14 @@
 let params = (new URL(document.location)).searchParams;
 //document.getElementsByTagName('h1')[0].innerHTML = params.get('pokemon');
+
+const tabs = document.querySelectorAll('.tab-item');
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', (e) => {
+        document.querySelector('.tab-item.active').classList.remove('active');
+        document.querySelector('.detail-tab.active').classList.remove('active');
+        tab.classList.add('active');
+        const targetTab = tab.dataset.target;
+        document.getElementById(targetTab).classList.add('active');
+    })
+});
